13c42f4d3a33f5a2bc42d6677372d83991d60114
Loic Chamard <lchamard@2A3.42angouleme.fr>
2026-06-02 16:44:44 +0200

    feat: add the script doc: explain installation

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # clean-norm42-tester
 A 42 norm tester who print stat on norm of your project
+## DOWNLOAD
+To download the script in your project please use the following command
+
+Clone the repo and chmod the script to make it executable.
+```
+git clone https://github.com/SeigneurLefou/clean-norm42-tester.git
+chmod +x clean-norm42-tester/*.sh
+```
+
+## EXECUTE
+Execute the script by using the following command
+```
+./clean-norm42-tester/count_norm_script.sh
+```
+
+Or by copying the script in your repo
+```
+cp clean-norm42-tester/count_norm_script.sh ./
+./count_norm_script.sh
+```
---
+++ count_norm_script.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+shopt -s nullglob
+
+TOTAL_LINE=$(cat *.c *.h **/*.c **/*.h 2> /dev/null | wc -l)
+if [ "$TOTAL_LINE" -eq 0 ]; then
+    echo "No .c or .h files"
+	exit 1
+fi
+TOTAL_ERROR=$(norminette | grep "Error:" | wc -l)
+TOTAL_RATIO=$(echo "scale=2; ($TOTAL_ERROR / $TOTAL_LINE) * 100" | bc)
+
+echo "======================================================"
+echo "📊 RATIO ERROR : $TOTAL_ERROR on $TOTAL_LINE lines"
+echo "📈 ${TOTAL_RATIO}%"
+echo "======================================================"
+echo ""
+
+check_norme_recursive()
+{
+    local target="$1"
+    local indent="$2"
+
+    if [[ "$target" == ".*" ]]; then
+		return
+    fi
+
+	local errs=$(norminette "$target" 2>/dev/null | grep "Error:" | wc -l)
+
+    local ratio="0%"
+    if [ "$TOTAL_ERROR" -gt 0 ]; then
+        ratio="$(( errs * 100 / TOTAL_ERROR ))%"
+    fi
+
+    if [ -d "$target" ]; then
+        if [ "$errs" -eq 0 ]; then
+            echo "${indent}✅ - $target/ -> 0 errors"
+            return
+        else
+            echo "${indent}❌ - $target/ -> $errs errors ($ratio du total)"
+
+            for child in "$target"/*; do
+                check_norme_recursive "$child" "$indent  "
+            done
+        fi
+
+    elif [ -f "$target" ] && [[ "$target" == *.c || "$target" == *.h ]]; then
+        if [ "$errs" -eq 0 ]; then
+            echo "${indent}✅ - $target -> 0 errors"
+        else
+            echo "${indent}❌ - $target -> $errs errors ($ratio du total)"
+        fi
+    fi
+}
+
+for item in *; do
+    check_norme_recursive "$item" ""
+done
