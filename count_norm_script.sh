32bc6fd4b558234b4deef3940e4e0cf23833d6ba
Loic Chamard <lchamard@2C4.42angouleme.fr>
2026-06-03 17:03:33 +0200

    feat:add flags for show or not the valid folder or file

--- count_norm_script.sh
+++ count_norm_script.sh
@@ -3,17 +3,25 @@
 shopt -s nullglob
 shopt -s globstar
 
+if [[ "$1" == "--show-valid" || "$1" == "-sv" ]]; then
+    VALID=1
+else
+    VALID=0
+fi
+
 TOTAL_LINE=$(cat **/*.c **/*.h *.c *.h 2>/dev/null | wc -l)
 if [ "$TOTAL_LINE" -eq 0 ]; then
     echo "No .c or .h files"
-	exit 1
+    exit 1
 fi
+
 TOTAL_ERROR=$(norminette | grep "Error:" | wc -l)
 TOTAL_RATIO=$(echo "scale=2; ($TOTAL_ERROR / $TOTAL_LINE) * 100" | bc)
+TOTAL_FILE=$(set -- **/*.c **/*.h; echo $#)
 
 echo "======================================================"
-echo "📊 RATIO ERROR : $TOTAL_ERROR on $TOTAL_LINE lines"
-echo "📈 ${TOTAL_RATIO}%"
+echo "📊 RATIO ERROR : $TOTAL_ERROR on $TOTAL_LINE lines for $TOTAL_FILE files"
+echo "📈 $TOTAL_RATIO%"
 echo "======================================================"
 echo ""
 
@@ -22,11 +30,12 @@ check_norme_recursive()
     local target="$1"
     local indent="$2"
 
-    if [[ "$target" == ".*" ]]; then
-		return
+    # Ignorer les fichiers cachés
+    if [[ "$(basename "$target")" == .* ]]; then
+        return
     fi
 
-	local errs=$(norminette "$target" 2>/dev/null | grep "Error:" | wc -l)
+    local errs=$(norminette "$target" 2>/dev/null | grep "Error:" | wc -l)
 
     local ratio="0%"
     if [ "$TOTAL_ERROR" -gt 0 ]; then
@@ -35,9 +44,11 @@ check_norme_recursive()
 
     if [ -d "$target" ]; then
         if [ "$errs" -eq 0 ]; then
-            echo "${indent}✅ - $target/ -> 0 errors"
+            if [ "$VALID" -eq 1 ]; then
+                echo "${indent}✅ - $target/ -> 0 errors"
+            fi
             return
-        else
+        else 
             echo "${indent}❌ - $target/ -> $errs errors ($ratio du total)"
 
             for child in "$target"/*; do
@@ -47,8 +58,10 @@ check_norme_recursive()
 
     elif [ -f "$target" ] && [[ "$target" == *.c || "$target" == *.h ]]; then
         if [ "$errs" -eq 0 ]; then
-            echo "${indent}✅ - $target -> 0 errors"
-        else
+            if [ "$VALID" -eq 1 ]; then
+                echo "${indent}✅ - $target -> 0 errors"
+            fi
+        else 
             echo "${indent}❌ - $target -> $errs errors ($ratio du total)"
         fi
     fi
